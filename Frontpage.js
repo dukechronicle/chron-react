052cddf357cb7f8269a87c40f8be52972b223821
Michael Lai <themichaellai@gmail.com>
2015-03-28 01:40:39 -0400

    Added Navigator

---
+++ Frontpage.js
@@ -0,0 +1,116 @@
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  ActivityIndicatorIOS,
+  NavigatorIOS,
+  Image,
+  ListView,
+  StyleSheet,
+  Text,
+  View,
+} = React;
+var API_URL = 'https://api.dukechronicle.com/qduke';
+
+var Frontpage = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2
+      }),
+      loaded: false
+    };
+  },
+  componentDidMount: function() {
+    this.fetchData();
+  },
+  fetchData: function() {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        var articlesMap = responseData.articles;
+        var newsPosts = responseData.layout.news.map((id) => articlesMap[id]);
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(newsPosts),
+          loaded: true
+        });
+      })
+      .done();
+  },
+  render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderPost}
+        style={styles.listView}
+      />
+    );
+  },
+  renderLoadingView: function() {
+    return (
+      <ActivityIndicatorIOS
+        hidden='true'
+        size='large'
+        style={styles.container}
+      />
+    );
+  },
+  renderPost: function(post) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: 'http:' + post.image.thumbnail_url}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={2} style={styles.title}>{post.title}</Text>
+          <Text numberOfLines={2} style={styles.teaser}>{post.teaser}</Text>
+        </View>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderColor: '#DDDDDD'
+  },
+  rightContainer: {
+    flex: 1,
+    paddingLeft: 10
+  },
+  title: {
+    fontSize: 15,
+    marginBottom: 8,
+    textAlign: 'left',
+    height: 35,
+    fontWeight: '600'
+  },
+  teaser: {
+    textAlign: 'left',
+    height: 35
+  },
+  thumbnail: {
+    width: 103,
+    height: 64
+  },
+  listView: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    backgroundColor: '#F5FCFF'
+  }
+});
+
+module.exports = Frontpage;
--- index.ios.js
+++ index.ios.js
@@ -3,113 +3,31 @@
 var React = require('react-native');
 var {
   AppRegistry,
-  ActivityIndicatorIOS,
-  Image,
-  ListView,
+  NavigatorIOS,
   StyleSheet,
-  Text,
-  View,
+  View
 } = React;
+
+var Frontpage = require('./Frontpage');
 
 var API_URL = 'https://api.dukechronicle.com/qduke';
 var chronreact = React.createClass({
-  getInitialState: function() {
-    return {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2
-      }),
-      loaded: false
-    };
-  },
-  componentDidMount: function() {
-    this.fetchData();
-  },
-  fetchData: function() {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((responseData) => {
-        var articlesMap = responseData.articles;
-        var newsPosts = responseData.layout.news.map((id) => articlesMap[id]);
-        this.setState({
-          dataSource: this.state.dataSource.cloneWithRows(newsPosts),
-          loaded: true
-        });
-      })
-      .done();
-  },
   render: function() {
-    if (!this.state.loaded) {
-      return this.renderLoadingView();
-    }
     return (
-      <ListView
-        dataSource={this.state.dataSource}
-        renderRow={this.renderPost}
-        style={styles.listView}
-      />
-    );
-  },
-  renderLoadingView: function() {
-    return (
-      <ActivityIndicatorIOS
-        hidden='true'
-        size='large'
+      <NavigatorIOS
         style={styles.container}
+        initialRoute={{
+          title: 'News',
+          component: Frontpage
+        }}
       />
     );
   },
-  renderPost: function(post) {
-    return (
-      <View style={styles.container}>
-        <Image
-          source={{uri: 'http:' + post.image.thumbnail_url}}
-          style={styles.thumbnail}
-        />
-        <View style={styles.rightContainer}>
-          <Text numberOfLines={2} style={styles.title}>{post.title}</Text>
-          <Text numberOfLines={2} style={styles.teaser}>{post.teaser}</Text>
-        </View>
-      </View>
-    );
-  }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-    paddingTop: 20,
-    paddingBottom: 20,
-    borderBottomWidth: 1,
-    borderColor: '#DDDDDD'
-  },
-  rightContainer: {
-    flex: 1,
-    paddingLeft: 10
-  },
-  title: {
-    fontSize: 15,
-    marginBottom: 8,
-    textAlign: 'left',
-    height: 35,
-    fontWeight: '600'
-  },
-  teaser: {
-    textAlign: 'left',
-    height: 35
-  },
-  thumbnail: {
-    width: 103,
-    height: 64
-  },
-  listView: {
-    paddingTop: 20,
-    paddingLeft: 15,
-    paddingRight: 15,
-    backgroundColor: '#F5FCFF'
+    flex: 1
   }
 });
 
