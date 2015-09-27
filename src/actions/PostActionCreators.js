697bfbcfe9352949977b7f91c8014af1026cdcbb
Catherine Zhang <catzhangy1@gmail.com>
2015-09-26 21:21:24 -0400

    GetSection returns promise in PostActionCreators

--- src/actions/PostActionCreators.js
+++ src/actions/PostActionCreators.js
@@ -43,10 +43,10 @@ const getSection = (section) => {
 
 const PostActionCreators = {
   getFrontpage: () => {
-    getSection('news');
+    return getSection('news');
   },
   getSection: (section) => {
-    getSection(section);
+    return getSection(section);
   },
 };
 
