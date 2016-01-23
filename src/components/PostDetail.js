ad657e85f7c822e385364689cefee440c5dcd918
Catherine Zhang <catzhangy1@gmail.com>
2016-01-23 17:24:23 -0500

    PostDetail updated

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,10 +1,7 @@
 const _ = require('underscore');
 const React = require('react-native');
 const {
-  Image,
   StyleSheet,
-  ScrollView,
-  Text,
   View,
   WebView,
 } = React;
@@ -57,62 +54,69 @@ const PostDetail = React.createClass({
     navigator: postPropTypes.isRequired,
   },
 
-  getHTML: function(){
-    const post = this.props.post;
-
-    const headHTML = '<!DOCTYPE html><html>';
-    const styleHTML = `<head> <style> ${innerStyles} </style> </head>`;
-    const titleHTML = `<p id='title'> ${post.title} </p>`;
-    const captionHTML = (post.images.length > 0 && post.images[0].caption !== '') ? `<p id='caption'> ${post.images[0].caption} </p>` : '';
-    const bylineHTML = (post.authors) ? `<p id='byline'>By ${post.authors.join(', ')} </p>` : '';
-    const imageHTML = (post.images.length > 0) ? `<img id='image' src=' ${post.images[0].previewUrl} '>` : '';
-    const contentHTML = post.body;
-    const embedHTML = this.getEmbedHTML(post.url);
-    const endHTML = '</html>';
-
-    return [
-      headHTML,
-      styleHTML,
-      titleHTML,
-      captionHTML,
-      bylineHTML,
-      imageHTML,
-      contentHTML,
-      embedHTML,
-      endHTML
-    ].join('');
+  getInitialState: function() {
+    return {
+      url: 'about:blank',
+      status: 'No Page Loaded',
+      backButtonEnabled: false,
+      forwardButtonEnabled: false,
+      loading: true,
+      scalesPageToFit: true,
+    };
   },
 
-  getEmbedHTML: function(url){
-    const sHTML = `<div id='disqus_thread'></div><script type ='text/javascript'>`;
-    const urlHTML = `var disqus_url ='${url}';`;
-    const jsHTML = `
+  getHTML: function() {
+    const post = this.props.post;
+
+    const caption = (post.images.length > 0 && post.images[0].caption !== '') ? post.images[0].caption : '';
+    const author = (post.authors) ? `<p id='byline'>By ${post.authors.join(', ')} </p>` : '';
+    const image = (post.images.length > 0) ? `<img id='image' src=' ${post.images[0].previewUrl} '>` : '';
+    const disqusHTML = `
+      <!DOCTYPE html>
+      <div id='disqus_thread'></div>
+      <script type='text/javascript'>
+        var disqus_url = '${post.url}';
         (function(){ var dsq = document.createElement('script');
         dsq.type = 'text/javascript';
         dsq.async = true;
         dsq.src = 'http://dukechronicle.disqus.com/embed.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq); })();
-        `;
-    const fHTML = `</script>`;
-    return `${sHTML} ${urlHTML} ${jsHTML} ${fHTML}`;
+      </script>
+    `;
+
+    return `
+      <!DOCTYPE html>
+      <html>
+      <head>
+      <style> ${innerStyles} </style>
+          <p id='title'> ${post.title} </p>
+          <p id='caption'> ${caption} </p>
+          <p id='byline'> ${author} </p>
+          ${image}
+          ${post.body}
+          ${disqusHTML}
+      </head>
+      </html>
+    `;
   },
 
-  redirect: function(navState){
-    if(navState.loading == true){
+  redirect: function(navState) {
+    if (navState.loading === true) {
       return;
     }
     const url = navState.url;
     const successes = [
-      "disqus.com/next/login-success",
-      "disqus.com/_ax/google/complete",
-      "disqus.com/_ax/twitter/complete",
-      "disqus.com/_ax/facebook/complete"
+      'disqus.com/next/login-success',
+      'disqus.com/_ax/google/complete',
+      'disqus.com/_ax/twitter/complete',
+      'disqus.com/_ax/facebook/complete',
     ];
-    if((url.split('.com'))[0].contains('disqus') && _.some(successes, (success) => {return url.contains(success)})){
+
+    if ((url.split('.com'))[0].contains('disqus') && _.some(successes, (success) => {return url.contains(success); })) {
       this.props.navigator.push({
         title: this.props.title,
         component: PostDetail,
-        passProps: {post: this.props.post, navigator: this.props.navigator}
+        passProps: {post: this.props.post, navigator: this.props.navigator},
       });
     }
   },
@@ -121,13 +125,14 @@ const PostDetail = React.createClass({
     return (
       <View style={styles.container}>
         <WebView
-          ref={WEBVIEW_REF}
-          html={this.getHTML()}
-          automaticallyAdjustContentInsets={false}
-          javaScriptEnabledAndroid={true}
-          startInLoadingState={false}
-          onNavigationStateChange={this.redirect}
-        />
+            ref={WEBVIEW_REF}
+            html={this.getHTML()}
+            automaticallyAdjustContentInsets={false}
+            javaScriptEnabledAndroid={true}
+            startInLoadingState={false}
+            onLoadEnd={this.onLoadEnd}
+            onNavigationStateChange={this.redirect}
+            />
       </View>
     );
   },
