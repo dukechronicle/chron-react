fc0f8c1773852cea9bf94fad6577a50151e0ea7d
blakejkaplan <blakejkaplan@gmail.com>
2016-03-15 15:44:37 -0400

    Upgraded React Native to Version 0.21

--- iOS/chronreact/AppDelegate.m
+++ iOS/chronreact/AppDelegate.m
@@ -9,7 +9,6 @@
 
 #import "AppDelegate.h"
 
-#import "RCTPushNotificationManager.h"
 #import "RCTRootView.h"
 
 @implementation AppDelegate
@@ -53,19 +52,6 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
   return YES;
-}
-
-// Required for the register event.
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-{
-  NSLog(@"AppDelegate didRegisterForRemoteNotificationsWithDeviceToken");
-  [RCTPushNotificationManager application:application didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
-}
-
-// Required for the notification event.
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)notification
-{
-  [RCTPushNotificationManager application:application didReceiveRemoteNotification:notification];
 }
 
 @end
